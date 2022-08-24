d53ade9cd814c5b102be492596c1f8ab1a84a0a6
David Sharpe <david.a.sharpe@cgi.com>
2022-08-24 13:14:51 -0700

    Attempt to enable DynamoDB configuration. Remove Organization controller for simplicity.

---
+++ src/main/java/ca/bc/hlth/mohorganizations/ProductInfoRepository.java
@@ -0,0 +1,13 @@
+package ca.bc.hlth.mohorganizations;
+
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+@EnableScan
+public interface ProductInfoRepository extends
+        CrudRepository<ProductInfo, String> {
+    
+    Optional<ProductInfo> findById(String id);
+}
