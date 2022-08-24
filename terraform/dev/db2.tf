cb95b22ecd9d4eb134063b2b96fb6dbcf626451c
David Sharpe <david.a.sharpe@cgi.com>
2022-08-24 14:28:17 -0700

    Correct Dynamodb attribute.

--- terraform/dev/db2.tf
+++ terraform/dev/db2.tf
@@ -1,6 +1,6 @@
 resource "aws_dynamodb_table" "startup_sample_table2" {
   name           = "ProductInfo"
-  hash_key       = "pid"
+  hash_key       = "id"
   read_capacity  = 1
   write_capacity = 1
 
