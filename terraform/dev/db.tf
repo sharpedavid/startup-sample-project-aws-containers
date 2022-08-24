baa9fc75ca50049110c81bfdc9a693280c13687a
David Sharpe <david.a.sharpe@cgi.com>
2022-08-24 14:05:25 -0700

    Remove new Terraform config to debug issue.

--- terraform/dev/db.tf
+++ terraform/dev/db.tf
@@ -1,42 +1,42 @@
-resource "aws_dynamodb_table" "startup_sample_table2" {
-  name           = "ProductInfo"
-  hash_key       = "pid"
-  read_capacity  = 1
-  write_capacity = 1
-
-  attribute {
-    name = "id"
-    type = "S"
-  }
-
-}
-
-resource "aws_iam_role_policy" "sample_app_dynamodb2" {
-  name = "sample_app_dynamodb"
-  role = aws_iam_role.sample_app_container_role.id
-
-  policy = <<-EOF
-    {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-          "Effect": "Allow",
-          "Action": [
-              "dynamodb:BatchGet*",
-              "dynamodb:DescribeStream",
-              "dynamodb:DescribeTable",
-              "dynamodb:Get*",
-              "dynamodb:Query",
-              "dynamodb:Scan",
-              "dynamodb:BatchWrite*",
-              "dynamodb:CreateTable",
-              "dynamodb:Delete*",
-              "dynamodb:Update*",
-              "dynamodb:PutItem"
-          ],
-          "Resource": "${aws_dynamodb_table.startup_sample_table2.arn}"
-        }
-    ]
-  }
-  EOF
-}
+#resource "aws_dynamodb_table" "startup_sample_table2" {
+#  name           = "ProductInfo"
+#  hash_key       = "pid"
+#  read_capacity  = 1
+#  write_capacity = 1
+#
+#  attribute {
+#    name = "id"
+#    type = "S"
+#  }
+#
+#}
+#
+#resource "aws_iam_role_policy" "sample_app_dynamodb2" {
+#  name = "sample_app_dynamodb"
+#  role = aws_iam_role.sample_app_container_role.id
+#
+#  policy = <<-EOF
+#    {
+#    "Version": "2012-10-17",
+#    "Statement": [
+#      {
+#          "Effect": "Allow",
+#          "Action": [
+#              "dynamodb:BatchGet*",
+#              "dynamodb:DescribeStream",
+#              "dynamodb:DescribeTable",
+#              "dynamodb:Get*",
+#              "dynamodb:Query",
+#              "dynamodb:Scan",
+#              "dynamodb:BatchWrite*",
+#              "dynamodb:CreateTable",
+#              "dynamodb:Delete*",
+#              "dynamodb:Update*",
+#              "dynamodb:PutItem"
+#          ],
+#          "Resource": "${aws_dynamodb_table.startup_sample_table2.arn}"
+#        }
+#    ]
+#  }
+#  EOF
+#}
